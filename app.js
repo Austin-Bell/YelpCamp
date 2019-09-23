bb52ecab20ada57904b008fa20a494cd3db109d2
Ubuntu <ubuntu@ip-172-31-31-152.us-east-2.compute.internal>
2019-09-23 19:36:33 +0000

    add mongo-lab

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ var commentRoutes = require("./routes/comments"),
      
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect("mongodb+srv://Austin:Swj3TbfQ7rUtzLe@cluster0-mnyob.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true})
+// mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true, useUnifiedTopology: true  });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
