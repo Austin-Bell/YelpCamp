72e128761df656212ed7a3a8dda350081cddb8ef
Ubuntu <ubuntu@ip-172-31-31-152.us-east-2.compute.internal>
2019-09-23 20:41:17 +0000

    update dburl

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
      
 
-
-mongoose.connect("mongodb+srv://Austin:Swj3TbfQ7rUtzLe@cluster0-mnyob.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true})
-// mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true, useUnifiedTopology: true  });
+// mongoose.connect("mongodb+srv://Austin:Swj3TbfQ7rUtzLe@cluster0-mnyob.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true, useUnifiedTopology: true  });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
